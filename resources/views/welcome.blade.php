39f5783a6d5d47522c2d02870a70e2270e8bf993
Leoprimorac <leoprimorac96@gmail.com>
2019-03-05 20:28:19 +0100

    Test gita

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laraveleeeeee
                 </div>
 
                 <div class="links">
