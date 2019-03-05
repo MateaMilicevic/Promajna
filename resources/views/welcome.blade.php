86c1fcc43615fcbf6afbff1e1ba28bf264731b34
Leoprimorac <leoprimorac96@gmail.com>
2019-03-05 20:56:30 +0100

    Test gita 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laraveleeeeee
+                    Promajna
                 </div>
 
                 <div class="links">
