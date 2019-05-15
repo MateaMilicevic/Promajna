f889687844fd128a922b4d94cdfa32513b0e5829
MateaMilicevic <mateaaa.milicevic@gmail.com>
2019-05-15 12:38:55 +0200

    Matea

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,97 +3,53 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <title>PROMAJNA</title>
+        <link href="{{ asset('css/welcome.css') }}" rel="stylesheet">
+        <link href="{{ asset('js/bootstrap.js') }}" rel="stylesheet">
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Promajna
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+        <div class="title m-b-md">
+            Promajna
         </div>
+        <div id="Navbar" class="row">
+            <nav id="myNavbar" class="navbar fixed-top navbar-toggleable-md navbar-light bg-faded">
+                    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                      <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                      <ul class="navbar-nav mr-auto">
+                        <li class="nav-item option"><a class="nav-link" href="#home">Početna</a></li>
+                        <a class="nav-link dropdown-toggle" href="#app" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Apartmani
+                              </a>
+                              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="#matea">Matea</a>
+                                <a class="dropdown-item" href="#karla">Karla</a>
+                                <a class="dropdown-item" href="#nikola">Nikola</a>
+                                <a class="dropdown-item" href="#gloria">Gloria</a>
+                                <div class="dropdown-divider"></div>
+                              </div>
+                        <li class="nav-item option"><a class="nav-link" href="#">Upit</a></li>
+                        <li class="nav-item option"><a class="nav-link" href="#">Apartman Dvije Palme</a></li>
+                        <li class="nav-item option"><a class="nav-link" href="#">O nama</a></li>
+                        <li class="nav-item option"><a class="nav-link" href="#">Kontakt</a></li>
+                    </div>
+                  </nav>
+        </div>
+                  <div class="container-fluid">
+                        <section id="home" class="row d-flex flex-column justify-content-center align-items-left">
+                            <div id="zaglavlje" class="row">
+                            <div id="slika" class="col-2 col-md-4 d-flex flex-column justify-content-left align-items-left">
+                                <div>
+
+                                <img src={{ asset('img/kuca.jpg') }}>
+                                </div>
+                            </div>
+                        </section>
+                        <div id="glavni" class="row">
+                            <div id="tekst" class="col-8 col-md-8 d-flex flex-column justify-content-center align-items-center">
+                                <section id="about" class="row d-flex flex-column justify-content-center align-items-left">
+
+
     </body>
 </html>
